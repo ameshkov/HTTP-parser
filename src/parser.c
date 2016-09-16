16583196436dfe31240646433bf652e770bf5d7f
Dmitry Dikun <dikundm@yandex.ru>
2016-09-16 17:42:20 +0300

    Request/Response switching.

--- src/parser.c
+++ src/parser.c
@@ -1,6 +1,6 @@
 /*
- *	HTTP parser internals.
- *	Based on http parser API from Node.js project. 
+ *  HTTP parser internals.
+ *  Based on http parser API from Node.js project. 
  */
 #include "nodejs_http_parser/http_parser.h"
 #include "parser.h"
@@ -11,127 +11,148 @@
 
 
 /*
- *	Debug helpers:
+ *  Debug helpers:
  */
 
 #define DBG_HTTP_CALLBACK \
- 	printf ("DEBUG: %s\n", __FUNCTION__); \
+     printf ("DEBUG: %s\n", __FUNCTION__); \
 
 #define DBG_HTTP_CALLBACK_DATA \
-	printf ("DEBUG: %s\n", __FUNCTION__); \
-	char *out = malloc ((length + 1) * sizeof(char)); \
-	memset (out, 0, length + 1); \
-	memcpy (out, at, length); \
-	printf("%s\n", out); \
-	free(out); \
+    printf ("DEBUG: %s\n", __FUNCTION__); \
+    char *out = malloc ((length + 1) * sizeof(char)); \
+    memset (out, 0, length + 1); \
+    memcpy (out, at, length); \
+    printf("%s\n", out); \
+    free(out); \
 
 #define DBG_PARSER_TYPE \
-	printf ("DEBUG: Parser type: %d\n", parser->type); 
+    printf ("DEBUG: Parser type: %d\n", parser->type); 
 
 /*
- *	Private stuff:
+ *  Private stuff:
  */
 
 typedef struct {
-	connection_id			id;
-	connection_info			*info;
-	parser_callbacks		*callbacks;
-	http_parser				*parser;
-	http_parser_settings 	*settings;
+    connection_id           id;
+    connection_info         *info;
+    parser_callbacks        *callbacks;
+    http_parser             *parser;
+    http_parser_settings    *settings;
 } connection_context;
 
+#define CONTEXT   ((connection_context*)parser->data)
+#define CALLBACKS ((CONTEXT)->callbacks)
+
 /*
- *	Node.js http_parser's callbacks (parser->settings)
+ *  Node.js http_parser's callbacks (parser->settings)
  */
 
 int _on_message_begin(http_parser *parser) {
-	return 0;
+    return 0;
 }
 
 int _on_url(http_parser *parser, const char *at, size_t length) {
-	return 0;
+    return 0;
 }
 
 int _on_status(http_parser *parser, const char *at, size_t length) {
-	return 0;
+    return 0;
 }
 
 int _on_header_field(http_parser *parser, const char *at, size_t length) {
-	return 0;
+    return 0;
 }
 
 int _on_header_value(http_parser *parser, const char *at, size_t length) {
-	return 0;
+    return 0;
 }
 
 int _on_headers_complete(http_parser *parser) {
-	return 0;
+    int skip = 0;
+    switch (parser->type) {
+        case HTTP_REQUEST:
+            skip = CALLBACKS->http_request_received(CONTEXT->id, NULL, 0);
+            break;
+        case HTTP_RESPONSE:
+            skip = CALLBACKS->http_response_received(CONTEXT->id, NULL, 0);
+            break;
+        default:
+            /**!-TODO: It's impossible in theory, some error handling needed... */
+            break;
+    }
+    /**!-TODO: Handle message skipping here. */
+    return 0;
 }
 
 int _on_body(http_parser *parser, const char *at, size_t length) {
-	return 0;
+    return 0;
 }
 
 int _on_message_complete(http_parser *parser) {
-	http_parser_init(parser, HTTP_BOTH);
-	return 0;
+    http_parser_init(parser, HTTP_BOTH);
+    return 0;
 }
 
 int _on_chunk_header(http_parser *parser) {
-	return 0;
+    return 0;
 }
 
 int _on_chunk_complete(http_parser *parser) {
-	return 0;
+    return 0;
 }
 
 
 http_parser_settings _settings = {
-	.on_message_begin		= _on_message_begin,
-	.on_url					= _on_url,
-	.on_status				= _on_status,
-	.on_header_field		= _on_header_field,
-	.on_header_value		= _on_header_value,
-	.on_headers_complete	= _on_headers_complete,
-	.on_body				= _on_body,
-	.on_message_complete	= _on_message_complete,
-	.on_chunk_header		= _on_chunk_header,
-	.on_chunk_complete		= _on_chunk_complete
+    .on_message_begin       = _on_message_begin,
+    .on_url                 = _on_url,
+    .on_status              = _on_status,
+    .on_header_field        = _on_header_field,
+    .on_header_value        = _on_header_value,
+    .on_headers_complete    = _on_headers_complete,
+    .on_body                = _on_body,
+    .on_message_complete    = _on_message_complete,
+    .on_chunk_header        = _on_chunk_header,
+    .on_chunk_complete      = _on_chunk_complete
 };
 
 connection_context *context = NULL;
 
 /*
- *	API implementaion:
+ *  API implementaion:
  */
 
-int connect(connection_id id, connection_info *info, parser_callbacks *callbacks) {
-	context = malloc(sizeof(connection_context));
-	
-	context->id = id;
-	context->info = info;
-	context->callbacks = callbacks;
+int connect(connection_id id, connection_info *info,
+            parser_callbacks *callbacks) {
+    context = malloc(sizeof(connection_context));
+    
+    context->id = id;
+    context->info = info;
+    context->callbacks = callbacks;
 
-	context->settings = &_settings;
-	context->parser = malloc(sizeof(http_parser));
-	
-	http_parser_init(context->parser, HTTP_BOTH);
+    context->settings = &_settings;
+    context->parser = malloc(sizeof(http_parser));
 
-	return 0;
+    context->parser->data = context;
+    
+    http_parser_init(context->parser, HTTP_BOTH);
+
+    return 0;
 }
 
 int disconnect(connection_id id, transfer_direction direction) {
-	return 0;
+    return 0;
 }
 
-int input(connection_id id, transfer_direction direction, const char *data, size_t length) {
-	size_t done;
-	done = http_parser_execute (context->parser, context->settings, data, length);
-	
-	printf ("http_parser_execute - parsed %ld/%ld bytes.\n", done, length);
-	return 0;
+int input(connection_id id, transfer_direction direction, const char *data,
+          size_t length) {
+    size_t done;
+    done = http_parser_execute (context->parser, context->settings,
+                                data, length);
+    
+    printf ("http_parser_execute - parsed %ld/%ld bytes.\n", done, length);
+    return 0;
 }
 
 int close(connection_id id) {
-	return 0;
+    return 0;
 }
--- src/parser.h
+++ src/parser.h
@@ -1,5 +1,5 @@
 /*
- *	HTTP parser API.
+ *  HTTP parser API.
  */
 #ifndef PARSER_H
 #define PARSER_H
@@ -11,59 +11,68 @@ extern "C" {
 #include <sys/types.h>
 
 /*
- *	Types:
+ *  Types:
  */
+typedef struct {
+    char *field;
+    char *value;
+} http_header_parameter;
+
+typedef struct {
+    http_header_parameter *params;
+} http_header;
 
 typedef unsigned long connection_id;
 
-/*	Connection is represented by two abstract endpoints, which are titled for 
-	easy identification by C-strings. */
+/*  Connection is represented by two abstract endpoints, which are titled for
+    easy identification by C-strings. */
 #define ENDPOINT_MAX 255
 
 typedef struct {
-	char endpoint_1[ENDPOINT_MAX];
-	char endpoint_2[ENDPOINT_MAX];
+    char endpoint_1[ENDPOINT_MAX];
+    char endpoint_2[ENDPOINT_MAX];
 } connection_info;
 
 
 typedef enum transfer_direction transfer_direction;
 enum transfer_direction {
-	IN 				= 0,	/* from endpoint_1 to endpoint_2. */
-	OUT				= 1		/* from endpoint_2 to endpoint_1. */
+    IN                 = 0,    /* from endpoint_1 to endpoint_2. */
+    OUT                = 1     /* from endpoint_2 to endpoint_1. */
 };
 
-/*	User-defined callbacks.
-	To inform parser about needed action, other then default "nothing to do", 
-	callback should return non-zero code;
-	In all other cases you should return 0. */
+/*  User-defined callbacks.
+    To inform parser about needed action, other then default "nothing to do", 
+    callback should return non-zero code;
+    In all other cases you should return 0. */
 typedef int (*parser_cb)(connection_id id, void *data, size_t length);
 
 typedef struct {
-	parser_cb http_request_received;
-	parser_cb http_request_body_started;
-	parser_cb http_request_body_data;
-	parser_cb http_request_body_finished;
-	parser_cb http_response_received;
-	parser_cb http_response_body_started;
-	parser_cb http_response_body_data;
-	parser_cb http_response_body_finished;
+    parser_cb http_request_received;
+    parser_cb http_request_body_started;
+    parser_cb http_request_body_data;
+    parser_cb http_request_body_finished;
+    parser_cb http_response_received;
+    parser_cb http_response_body_started;
+    parser_cb http_response_body_data;
+    parser_cb http_response_body_finished;
 } parser_callbacks;
 
 /*
- *	General parser interface:
+ *  General parser interface:
  */
 
 int connect(connection_id id, connection_info *info,
-			parser_callbacks *callbacks);
+            parser_callbacks *callbacks);
 
 int disconnect(connection_id id, transfer_direction direction);
 
-int input(connection_id id, transfer_direction direction, const char *data, size_t length);
+int input(connection_id id, transfer_direction direction, const char *data,
+          size_t length);
 
 int close(connection_id id);
 
 /*
- *	Utility methods
+ *  Utility methods
  */
 
 /* To be continued... */
